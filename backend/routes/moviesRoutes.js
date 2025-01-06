fea55fdde6dc1bae141aef71c5e2972262368fd6
Josip Čanađija <43499613+JosipCanadija@users.noreply.github.com>
2025-01-06 18:05:31 +0100

    Added backend delete comment

--- backend/routes/moviesRoutes.js
+++ backend/routes/moviesRoutes.js
@@ -1,7 +1,15 @@
 import express from "express";
 const router = express.Router();
 
-import  {createMovie, getAllMovies, getSpecificMovie, updateMovie, movieReview, deleteMovie } from '../controllers/movieController.js';
+import  {
+    createMovie, 
+    getAllMovies, 
+    getSpecificMovie, 
+    updateMovie, 
+    movieReview, 
+    deleteMovie,
+    deleteComment
+ } from '../controllers/movieController.js';
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 import checkId from "../middlewares/checkId.js";
 
@@ -13,5 +21,6 @@ router.post('/:id/reviews', authenticate, checkId, movieReview)
 router.post('/create-movie', authenticate, authorizeAdmin, createMovie);
 router.put('/update-movie/:id', authenticate, authorizeAdmin, updateMovie);
 router.delete('/delete-movie/:id', authenticate, authorizeAdmin, deleteMovie);
+router.delete('/delete-comment/', authenticate, authorizeAdmin, deleteComment);
 
 export default router;
