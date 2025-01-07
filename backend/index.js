abf47104cd8a733bd3b0590b5bf121b129eb19d0
Josip Čanađija <43499613+JosipCanadija@users.noreply.github.com>
2025-01-07 16:52:06 +0100

    Added backend for image upload

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 import genreRoutes from './routes/genreRoutes.js';
 import moviesRoutes from './routes/moviesRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
 dotenv.config();
 connectDB();
@@ -23,5 +24,9 @@ const PORT = process.env.PORT || 3000;
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/genre', genreRoutes);
 app.use('/api/v1/movies', moviesRoutes);
+app.use('/api/v1/upload', uploadRoutes);
+
+const _dirname = path.resolve();
+app.use('/uploads', express.static(path.join(_dirname + '/uploads')));
 
 app.listen(PORT, () => console.log('Server is running on port ${PORT}'));
