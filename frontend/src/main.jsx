5f846598976a9b9386952c2a4f20eb5ab9615316
Josip Čanađija <43499613+JosipCanadija@users.noreply.github.com>
2025-01-25 21:30:59 +0100

    Added admin dashboard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -17,6 +17,7 @@ import UpdateMovie from "./pages/Admin/UpdateMovie.jsx";
 import AllMovies from './pages/Movies/AllMovies.jsx';
 import MovieDetails from './pages/Movies/MovieDetails.jsx'
 import AllComments from './pages/Admin/AllComments.jsx';
+import AdminDashboard from './pages/Admin/Dashboard/AdminDashboard.jsx'
 
 //Restricted
 import Login from './pages/Auth/Login.jsx';
@@ -44,7 +45,10 @@ const router = createBrowserRouter(
                 <Route path='/admin/movies/create' element={<CreateMovie />} />
                 <Route path='/admin/movies-list' element={<AdminMoviesList />} />
                 <Route path='/admin/movies/update/:id' element={<UpdateMovie />} />
+                <Route path='/admin/movies/dashboard' element={<AdminDashboard />} />
                 <Route path='/admin/movies/comments' element={<AllComments />} />
+
+
 
             </Route>
         </Route>
---
+++ frontend/src/pages/Admin/Dashboard/AdminDashboard.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "./Sidebar/Sidebar";
+import Main from "./Main/Main";
+
+const AdminDashboard = () => {
+    return (
+    <>
+        <Sidebar />
+        <Main />
+    </>
+  );
+};
+
+export default AdminDashboard;
